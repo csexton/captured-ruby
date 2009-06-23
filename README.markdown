be86d33122de9e0d49c34b47e882155b7c060234
Christopher Sexton <csexton@gmail.com>
2009-06-22 23:13:47 -0400

    Added grephics and tweaked growl

--- README.markdown
+++ README.markdown
@@ -1,7 +1,16 @@
-= captured
+captured
+========
 
-Description goes here.
+Quick screen capture and sharing application for Mac OS X.
 
-== Copyright
+
+Icon
+====
+
+Icons from the [Crystal Clear](http://www.everaldo.com/crystal/) icon set by [Everaldo Coelho](http://en.wikipedia.org/wiki/Everaldo_Coelho). – The icons are [licensed](http://www.everaldo.com/crystal/?action=license) under the [GNU Lesser General Public License (LGPL)](http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License).
+
+
+Copyright
+=========
 
 Copyright (c) 2009 Christopher Sexton. See LICENSE for details.
--- lib/captured/file_uploader.rb
+++ lib/captured/file_uploader.rb
@@ -4,8 +4,11 @@ class FileUploader
   def self.upload(file)
     remote_name = Digest::MD5.hexdigest(file)
     puts "Uploading #{file}, remote name #{remote_name}!"
-    system "scp '#{file}' 'fuzzymonk.com:fuzzymonk.com/captured/#{remote_name}.png'"
-    `echo "http://fuzzymonk.com/captured/#{remote_name}.png" | pbcopy`
-    `growlnotify -t "Captured" -m "Uploaded screenshot"`
+    if system "scp '#{file}' 'fuzzymonk.com:fuzzymonk.com/captured/#{remote_name}.png'"
+      `echo "http://fuzzymonk.com/captured/#{remote_name}.png" | pbcopy`
+      `growlnotify -t "Captured" -m "Uploaded screenshot" --image "#{File.dirname(File.expand_path(__FILE__))}/../../resources/captured.png"`
+    else
+      `growlnotify -t "Captured" -m "Uploaded failed." --image "#{File.dirname(File.expand_path(__FILE__))}/../../resources/red_x.png"`
+    end
   end
 end
