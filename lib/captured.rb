4b3361b1750b7246e1f3424511807785e834cc7c
Christopher Sexton <csexton@gmail.com>
2009-06-26 19:29:50 -0400

    Adding guess OS version.

--- lib/captured.rb
+++ lib/captured.rb
@@ -6,6 +6,17 @@ class Captured
     "#{ENV['HOME']}/.captured.yml" 
   end
 
+  def self.guess_watch_path
+    if `sw_vers | awk '/ProductVersion:/ {print $2}'`.strip.start_with? "10.5"
+      "Picture**.png"
+    else
+      "Screenshot**.png"
+    end
+  rescue => e
+    puts e
+    "Screenshot**.png"
+  end
+
   def self.run_once!(options)
     watch_path = options[:watch_path] || "#{ENV['HOME']}/Desktop/"
     Dir["#{watch_path}#{options[:watch_pattern]}"].each do |file|
