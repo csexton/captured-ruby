d236a31f30622131d5a48b7d89adae672e27df25
Christopher Sexton <csexton@gmail.com>
2010-01-11 16:47:08 -0500

    Ability to disable integration tests with config file

--- spec/uploader_specs/imageshack_uploader_spec.rb
+++ spec/uploader_specs/imageshack_uploader_spec.rb
@@ -1,14 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/captured/uploaders/imageshack_uploader')
 
-describe "SCP File Uploader" do
-  it "should upload to the server" do
-    config = {"upload"=>{"url"=>"http://fuzzymonk.com/captured/",
-      "type"=>"imageshack",
-      "shackid"=>"capturedspec"}}
+if CONFIG['imageshack_spec']
+  describe "Imageshack File Uploader" do
+    it "should upload to the server" do
+      config = {"upload"=>{"url"=>"http://fuzzymonk.com/captured/",
+        "type"=>"imageshack",
+        "shackid"=>"capturedspec"}}
 
-    @uploader = ImageshackUploader.new(config)
-    @uploader.upload(File.expand_path(File.dirname(__FILE__) + '/../../resources/captured.png'))
-    system "open #{@uploader.url}"
+      @uploader = ImageshackUploader.new(config)
+      @uploader.upload(File.expand_path(File.dirname(__FILE__) + '/../../resources/captured.png'))
+      system "open #{@uploader.url}"
+    end
   end
 end
--- spec/uploader_specs/scp_uploader_spec.rb
+++ spec/uploader_specs/scp_uploader_spec.rb
@@ -1,13 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/captured/uploaders/scp_uploader')
 
-describe "SCP File Uploader" do
-  it "should upload to the server" do
-    # This spec requires a scp_spec section in the config file with the
-    # scp settings
-    config = YAML.load_file("#{ENV['HOME']}/.captured.yml")['scp_spec']
-    @uploader = ScpUploader.new(config)
-    @uploader.upload(File.expand_path(File.dirname(__FILE__) + '/../../resources/captured.png'))
-    system "open #{@uploader.url}"
+if CONFIG['scp_spec']
+  describe "SCP File Uploader" do
+    it "should upload to the server" do
+      # This spec requires a scp_spec section in the config file with the
+      # scp settings
+      config = YAML.load_file("#{ENV['HOME']}/.captured.yml")['scp_spec']
+      @uploader = ScpUploader.new(config)
+      @uploader.upload(File.expand_path(File.dirname(__FILE__) + '/../../resources/captured.png'))
+      system "open #{@uploader.url}"
+    end
   end
 end
